7c6f63477ed1f15bbc5b6d251b9c34a936975f1f
Sofia Bonakdar <sofia@bonakdar.se>
2021-05-20 11:49:33 +0200

    sent

---
+++ index.js
@@ -0,0 +1,122 @@
+const express = require("express");
+const app = express();
+const shuffle = require("shuffle-array");
+const path = require("path");
+const axios = require("axios");
+const http = require("http");
+const httpServer = http.createServer(app);
+
+const io = require("socket.io")(httpServer);
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+let player = undefined;
+let questions = undefined;
+let index = 0;
+
+io.on("connect", (socket) => {
+  console.log(`A client with id ${socket.id} connected!`);
+
+  function sendNextQuestion() {
+    if (index <= 4) {
+      io.in("playerRoom").emit("messageP", {
+        question: `Qestion ${index + 1}: ${questions[index].question}`,
+        answers: questions[index].answers,
+      });
+      io.in("viewerRoom").emit("messageV", {
+        question: `Qestion ${index + 1}: ${questions[index].question}`,
+      });
+    } else {
+      io.emit("points", {
+        correct,
+        incorrect,
+      });
+      correct = 0;
+      incorrect = 0;
+    }
+  }
+
+  if (player === undefined) {
+    socket.join("playerRoom");
+    socket.emit("player", "You are the player!");
+    io.in("viewerRoom").emit("con", "player connected!");
+    player = socket.id;
+    Question().then((res) => {
+      questions = res;
+      sendNextQuestion();
+    });
+  } else {
+    socket.join("viewerRoom");
+    socket.emit("player", "player is connected!");
+    if (questions) {
+      io.to(socket.id).emit("messageV", {
+        question: `Qestion ${index + 1}: ${questions[index].question}`,
+      });
+    }
+  }
+  if (player == !undefined) {
+    io.in("viewerRoom").emit("con", "player connected!");
+  }
+  async function Question() {
+    try {
+      const response = await axios.get(
+        "https://opentdb.com/api.php?amount=5&type=multiple"
+      );
+      let questions = [];
+
+      response.data.results.forEach((obj) => {
+        let answers = [];
+        let question = obj.question;
+        let correctAnswer = obj.correct_answer;
+        answers.push(correctAnswer);
+        obj.incorrect_answers.forEach((e) => {
+          answers.push(e);
+        });
+        shuffle(answers);
+
+        questions.push({
+          question,
+          answers,
+          correctAnswer,
+        });
+      });
+      return questions;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  let correct = 0;
+  let incorrect = 0;
+
+  socket.on("answer", (ans) => {
+    if (ans === questions[index].correctAnswer) {
+      socket.to("viewerRoom").emit("correctedAns", `${ans}(correct)`);
+      correct++;
+      index++;
+    } else {
+      socket.to("viewerRoom").emit("correctedAns", `${ans}(incorrect)`);
+      incorrect++;
+      index++;
+    }
+    if (index === 5) {
+      io.in("playerRoom").emit("end", "Finished!");
+    }
+    sendNextQuestion();
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`A client with id ${socket.id} diconnected!`);
+
+    if (player === socket.id) {
+      player = undefined;
+      socket.leave("playerRoom");
+      socket.to("viewerRoom").emit("dis", "Player disconnected");
+      index = 0;
+    }
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
---
+++ public/client.js
@@ -0,0 +1,54 @@
+const socket = io();
+
+socket.on("messageV", (response) => {
+  const h = document.createElement("h2");
+  document.getElementById("questions").appendChild(h);
+  h.textContent = response.question;
+});
+
+socket.on("messageP", (response) => {
+  document.getElementById("question").textContent = response.question;
+  document.getElementById("questions").innerHTML = "";
+  response.answers.forEach((answer) => {
+    const btn = document.createElement("button");
+    document.getElementById("questions").appendChild(btn);
+    btn.innerHTML = answer;
+    btn.class = "btn";
+    btn.type = "submit";
+    btn.style = "margin:5px";
+    btn.onclick = function answer() {
+      socket.emit("answer", btn.innerHTML);
+    };
+  });
+});
+
+socket.on("correctedAns", (ans) => {
+  const span = document.createElement("span");
+  document.getElementById("questions").appendChild(span);
+  span.textContent = `Player answered: ${ans}`;
+});
+
+socket.on("player", (msg) => {
+  document.getElementById("player").textContent = msg;
+});
+socket.on("points", (res) => {
+  document.getElementById(
+    "point"
+  ).innerHTML = `Correct answers: ${res.correct} <br/>
+                                                Incorrect answers: ${res.incorrect}`;
+});
+
+socket.on("dis", (res) => {
+  document.getElementById("questions").innerHTML = "";
+  document.getElementById("player").textContent = res;
+});
+socket.on("finalMsg", (res) => {
+  document.getElementById("questions").innerHTML = res;
+});
+socket.on("con", (res) => {
+  document.getElementById("player").textContent = res;
+});
+socket.on("end", (res) => {
+  document.getElementById("question").textContent = res;
+  document.getElementById("questions").innerHTML = "";
+});
